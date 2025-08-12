f024107f42218d85978ce141c47a75186b2ae2bd
rlopez00 <investments320@gmail.com>
2025-08-12 15:03:27 -0600

    chore(terraform): backend to us-east-1 + fix ecr + ssm overwrite

--- infra/providers.tf
+++ infra/providers.tf
@@ -6,8 +6,16 @@ terraform {
       version = "~> 5.60"
     }
   }
+
+  backend "s3" {
+    bucket         = "demo-devops-tfstate-924842505079-us-east-1"
+    key            = "infra/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "demo-devops-tf-locks"
+    encrypt        = true
+  }
 }
 
 provider "aws" {
-  region = var.region
+  region = var.region   # keep var.region = "us-east-1"
 }
--- infra/ssm.tf
+++ infra/ssm.tf
@@ -2,16 +2,17 @@ resource "aws_ssm_parameter" "db_username" {
   name  = "/${var.project_name}/db_username"
   type  = "String"
   value = var.db_username
+  overwrite = true
 }
-
 resource "aws_ssm_parameter" "db_password" {
   name  = "/${var.project_name}/db_password"
   type  = "SecureString"
   value = var.db_password
+  overwrite = true
 }
-
 resource "aws_ssm_parameter" "db_name" {
   name  = "/${var.project_name}/db_name"
   type  = "String"
   value = var.db_name
+  overwrite = true
 }
